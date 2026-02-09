add9196186a3deeb226cd56403b14c1cbb2f3218
Manish Vasu <manishvasubiz@gmail.com>
2026-02-09 22:59:34 +0530

    implements basic page read and write

---
+++ cmd/evergreen/main.go
@@ -0,0 +1,95 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"io/fs"
+	"log/slog"
+	"os"
+	"path/filepath"
+)
+
+const (
+	PageSize = 8 * 1024
+	DataDir  = "/var/lib/evergreen"
+)
+
+type Page struct {
+	buf [PageSize]byte
+}
+
+func NewPage() *Page {
+	return &Page{}
+}
+
+func (p *Page) Bytes() []byte {
+	return p.buf[:]
+}
+
+type Storage interface {
+	ReadPage(p *Page, pageNum int64) error
+	WritePage(p *Page, pageNum int64) error
+}
+
+type FileStore struct {
+	f *os.File
+}
+
+func NewFileStore(name string) (*FileStore, error) {
+	path := filepath.Join(DataDir, name)
+
+	if _, err := os.Stat(path); err == nil {
+		return nil, fmt.Errorf("index '%s' already exists", name)
+	} else if !errors.Is(err, fs.ErrNotExist) {
+		return nil, fmt.Errorf("failed to check existence of index: %w", err)
+	}
+
+	f, err := os.Create(path)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create index file: %w", err)
+	}
+	slog.Info("new index created", "name", name)
+
+	return &FileStore{f}, nil
+}
+
+func (fs *FileStore) ReadPage(p *Page, pageNum int64) error {
+	off := PageSize * pageNum
+
+	n, err := fs.f.ReadAt(p.Bytes(), off)
+	if err != nil {
+		return err
+	}
+	if n != PageSize {
+		return fmt.Errorf("short read: %d", n)
+	}
+
+	return nil
+}
+
+func (fs *FileStore) WritePage(p *Page, pageNum int64) error {
+	if len(p.Bytes()) != PageSize {
+		return fmt.Errorf("invalid page size: got %d bytes", len(p.Bytes()))
+	}
+
+	off := PageSize * pageNum
+	n, err := fs.f.WriteAt(p.Bytes(), off)
+	if err != nil {
+		return err
+	}
+
+	if n != PageSize {
+		return fmt.Errorf("short write: expected %d bytes, got %d", PageSize, n)
+	}
+
+	return nil
+}
+
+func main() {
+	f, _ := NewFileStore("test")
+	p := NewPage()
+	f.WritePage(p, 0)
+	p2 := NewPage()
+	f.WritePage(p2, 0)
+	fmt.Print(p2)
+}
